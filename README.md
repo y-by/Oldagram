763e27c4e92b2accd1673d6ff85af12294428009
janay <janayby@gmail.com>
2022-10-26 09:58:15 +0300

    Add all files

---
+++ README.md
@@ -0,0 +1,2 @@
+# As part of my re-learning the basics of web development
+Today Old version of instagram is on the line, i use .map to populate the cards.
---
+++ main.js
@@ -0,0 +1,52 @@
+const posts = [
+  {
+      name: "Vincent van Gogh",
+      username: "vincey1853",
+      location: "Zundert, Netherlands",
+      avatar: "images/avatar-vangogh.jpg",
+      post: "images/post-vangogh.jpg",
+      comment: "just took a few mushrooms lol",
+      likes: 21
+  },
+  {
+      name: "Gustave Courbet",
+      username: "gus1819",
+      location: "Ornans, France",
+      avatar: "images/avatar-courbet.jpg",
+      post: "images/post-courbet.jpg",
+      comment: "i'm feelin a bit stressed tbh",
+      likes: 4
+  },
+      {
+      name: "Joseph Ducreux",
+      username: "jd1735",
+      location: "Paris, France",
+      avatar: "images/avatar-ducreux.jpg",
+      post: "images/post-ducreux.jpg",
+      comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+      likes: 152
+  }
+]
+
+// const box =  document.querySelector('.box')
+
+document.getElementById('cards-place').innerHTML = posts.map(post =>
+  `
+  <div class="card">
+    <div class="card-header">
+      <img class="user-img" src="${post.avatar}" alt="Vincent Van Gogh in a circle">
+      <div class="info-container">
+        <div class="name"><p>${post.name}</p></div>
+        <div class="location"><p>${post.location}</p></div>
+      </div>    
+    </div>
+    <img class="post-img" src="${post.post}" alt="" srcset="">
+    <div class="post-icons">
+      <img class="post-icon" src="/images/icon-heart.png" alt="heart icon">
+      <img class="post-icon" src="/images/icon-comment.png" alt="comment icon">
+      <img class="post-icon" src="/images/icon-dm.png" alt="dm icon">
+    </div>
+    <div class="post-likes"><p>${post.likes} likes</p></div>
+    <div class="post-comments"><p><span>${post.username}</span> ${post.comment}</p></div>
+  </div>
+  `).join('')
